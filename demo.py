356f86edec9ec72b9e5000b938b4b5383e24bd87
whysamay <samaypawar2200@gmail.com>
2026-05-18 16:48:48 +0530

    logger file created

---
+++ demo.py
@@ -0,0 +1,8 @@
+# below code is to check the logging config
+from src.logger import logging
+
+logging.debug("This is a debug message.")
+logging.info("This is an info message.")
+logging.warning("This is a warning message.")
+logging.error("This is an error message.")
+logging.critical("This is a critical message.")
