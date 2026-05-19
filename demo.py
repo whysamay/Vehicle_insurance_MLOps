71c2cb920924069cd93298b56e46c73d735d3b30
whysamay <samaypawar2200@gmail.com>
2026-05-19 12:56:56 +0530

    DataIngestion Step 17 -> src: data ingestion, constants ,configuration.mongodbconnection, dataaccess.proj1data, entity. configs, entity.artifact, pipeline.training_pipeline. demo.py created/updated

--- demo.py
+++ demo.py
@@ -8,12 +8,17 @@
 # logging.critical("This is a critical message.")
 
 # below code is to check the exception config
-from src.logger import logging
-from src.exception import MyException
-import sys
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
